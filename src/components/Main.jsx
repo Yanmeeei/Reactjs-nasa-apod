406a9200a486cb1da0c7a5076141765564d28f54
Yanmeeei <wyanmei@umich.edu>
2024-11-24 13:12:21 -0500

    support video

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,25 @@
 export default function Main(props) {
     const {data} = props
     return (
-        <div className="image-container">
-            <img src={data? data.hdurl : "src/assets/Rose.png"} alt={data?.title} className="bgimage"/>
+        <div className="media-container">
+            {data && data.media_type !== 'video' ? (
+                <img
+                    src={data ? data.hdurl : "src/assets/Rose.png"}
+                    alt={data?.title || "Default image"}
+                    className="bgimage"
+                />
+            ) : data && data.media_type === 'video' ? (
+                <div className="iframe-wrapper">
+                    <iframe
+                        src={data.url}
+                        className="ytvideo"
+                        loading="lazy"
+                        title={data?.title || "Video content"}
+                        allowFullScreen
+                    />
+                </div>
+            ) : null}
         </div>
+
     )
 }
